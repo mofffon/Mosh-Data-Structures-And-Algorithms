0bfffa388747b6b2f394e49e6c693a0e4d1d4001
Lukasz Jaskiewicz <lukasz.jaskiewicz2@gmail.com>
2025-11-20 21:16:03 +0100

    Initial commit

---
+++ Part_1/src/main/java/Array.java
@@ -0,0 +1,89 @@
+public class Array {
+
+    private int[] items;
+    private int currentSize;
+
+    public Array(int size) {
+        items = new int[size];
+        currentSize = 0;
+    }
+
+    public void add(int item){
+        items[currentSize++] = item;
+        resize();
+    }
+
+    public int getCurrentSize(){
+        return currentSize;
+    }
+
+    public int removeAt(int index){
+        int counter = 0;
+        int[] newItems = new int[items.length];
+        int item = items[currentSize - 1];
+
+        for(int i = 0 ; i < currentSize; i++){
+            if(i == index){
+                continue;
+            }
+
+            newItems[counter++] = items[i];
+        }
+
+        currentSize--;
+        items = newItems;
+        resize();
+
+        return item;
+    }
+
+    public int indexOf(int item){
+
+        for(int i = 0; i < currentSize; i++){
+            if(items[i] == item){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private void resize(){
+        int[] newItems;
+        if(currentSize < items.length / 2){
+            newItems = new int[items.length / 2];
+        }else if(currentSize == items.length){
+            newItems = new int[items.length * 2];
+        }else{
+            return;
+        }
+
+        System.arraycopy(items, 0, newItems, 0, currentSize);
+
+        items = newItems;
+    }
+
+    public void print(){
+
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder("[");
+
+        String delimiter = ", ";
+
+        for(int i = 0; i < currentSize; i++){
+            if(i == currentSize - 1){
+                delimiter = "";
+            }
+
+            sb.append(items[i]).append(delimiter);
+        }
+
+        sb.append("]");
+
+        return sb.toString();
+    }
+}
---
+++ Part_1/src/test/java/ArrayTest.java
@@ -0,0 +1,119 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayTest {
+
+    @org.junit.jupiter.api.Test
+    void whenCalled_addWith_10_20_30_itShouldReturnArrOf_10_20_30() {
+
+        // Arrange
+        Array arr = new Array(2);
+
+        // Act
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+
+        // Assert
+        assertEquals("[10, 20, 30]", arr.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void whenCalled_addWith_10_20_30_itShouldReturnArrOf_10_20() {
+
+        // Arrange
+        Array arr = new Array(2);
+
+        // Act
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+
+        arr.removeAt(arr.getCurrentSize() -1);
+        arr.removeAt(arr.getCurrentSize() -1);
+
+        // Assert
+        assertEquals("[10, 20]", arr.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void whenCalled_removeAtIndex0_shouldReturn10() {
+
+        // Arrange
+        Array arr = new Array(1);
+
+        // Act
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+
+        // Assert
+        assertEquals(10, arr.removeAt(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void whenCalled_removeAtIndex1_shouldReturn20() {
+
+        // Arrange
+        Array arr = new Array(1);
+
+        // Act
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+
+        // Assert
+        assertEquals(20, arr.removeAt(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void whenCalled_removeAtIndex2_shouldReturn30() {
+
+        // Arrange
+        Array arr = new Array(1);
+
+        // Act
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+
+        // Assert
+        assertEquals(30, arr.removeAt(2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void whenCalled_indexOf10_20_30_shouldReturn0_1_2() {
+
+        // Arrange
+        Array arr = new Array(1);
+
+        // Act
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+
+        assertEquals(0, arr.indexOf(10));
+        assertEquals(1, arr.indexOf(20));
+        assertEquals(2, arr.indexOf(30));
+    }
+
+    @org.junit.jupiter.api.Test
+    void whenCalled_indexOf10_20_30_shouldReturn0_1_minus1() {
+
+        // Arrange
+        Array arr = new Array(1);
+
+        // Act
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+
+        arr.removeAt(arr.getCurrentSize() - 1);
+        arr.removeAt(arr.getCurrentSize() - 1);
+
+        assertEquals(0, arr.indexOf(10));
+        assertEquals(1, arr.indexOf(20));
+        assertEquals(-1, arr.indexOf(30));
+    }
+}
