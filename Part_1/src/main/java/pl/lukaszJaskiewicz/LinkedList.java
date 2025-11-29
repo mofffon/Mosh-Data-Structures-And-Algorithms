334ab625b3c7312dc7df502542ffb5e2c385c66d
Lukasz Jaskiewicz <lukasz.jaskiewicz2@gmail.com>
2025-11-29 13:05:49 +0100

    Part_1: Add LinkedList Exercises

---
+++ Part_1/src/main/java/pl/lukaszJaskiewicz/LinkedList.java
@@ -0,0 +1,318 @@
+package pl.lukaszJaskiewicz;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+
+    private static class Node {
+        int item;
+        Node next;
+
+        public Node (int item){
+            this.item = item;
+        }
+
+        public Node(int item, Node next){
+            this. item = item;
+            this.next = next;
+        }
+
+        @Override
+        public String toString(){
+            return Integer.toString(item);
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int size = 0;
+
+    public void addFirst(int item){
+
+        Node newNode = new Node(item);
+        newNode.next = first;
+
+        if(isEmpty()) {
+            last = newNode;
+        }
+
+        first = newNode;
+        size++;
+    }
+
+    public void addLast(int item) {
+
+        if(isEmpty()){
+            addFirst(item);
+            return;
+        }
+
+        Node newNode = new Node(item);
+
+        last.next = newNode;
+        last = newNode;
+        size++;
+    }
+
+    public void deleteFirst() {
+
+        if(isEmpty()) {
+            throw new NoSuchElementException("Can not deleteFirst() out of empty list.");
+        }
+
+        var current = first;
+        first = first.next;
+        current.next = null;
+        size--;
+    }
+
+    public void deleteLast() {
+        if (isEmpty()) {
+             throw new NoSuchElementException("Can not deleteLast() out of empty list.");
+        }
+
+        if(size == 1){
+            deleteFirst();
+            return;
+        }
+
+        last = getPrevious(last);
+
+        assert last != null;
+        last.next = null;
+        size--;
+    }
+
+    public int indexOf(int item){
+        Node current = first;
+
+        for(int i = 0; i < size; i++) {
+            if(current.item == item){
+                return i;
+            }
+
+            current = current.next;
+        }
+
+        return -1;
+    }
+
+    public boolean contains(int item) {
+        return indexOf(item) >= 0;
+    }
+
+    public int getKthFromTheEnd(int k){
+
+        int lSize = 0;;
+        if( k <= lSize){
+            return last.item;
+        }
+
+        if(isEmpty()){
+            throw new IllegalStateException("The list is empty.");
+        }
+
+        Node start = first;
+        Node end = first;
+        int counter = 0;
+
+
+        while(end != null){
+            lSize++;
+
+            if(counter < k){
+                counter++;
+            }else{
+                start = start.next;
+            }
+
+            end = end.next;
+        }
+
+        if(k > lSize){
+            throw new IllegalArgumentException("the k parameter is greater than list size (" + size + ")");
+        }
+
+        assert start != null;
+        return start.item;
+    }
+
+    public void reverse() {
+        if(isEmpty()){
+            return;
+        }
+
+        Node current = first;
+        Node previous = null;
+
+        while(current != null){
+            Node next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        Node temp = first;
+        first = last;
+        last = temp;
+
+        assert last != null;
+        last.next = null;
+    }
+
+    public int[] toArray() {
+        int[] array = new int[size];
+        var current = first;
+        for(int i = 0; i < size; i++){
+            array[i] = current.item;
+            current = current.next;
+        }
+
+        return array;
+    }
+
+    public void printMiddle() {
+        Integer[] middle = getMiddle();
+        System.out.println(Arrays.toString(middle));
+    }
+
+    public Integer[] getMiddle(){
+        Node fasterPointer = first;
+        Node slowerPointer = first;
+        Node slowerSlowerPointer = null;
+
+        int counter = 0;
+
+        while(fasterPointer != null){
+            if(counter % 2 == 1){
+                slowerSlowerPointer = slowerPointer;
+                slowerPointer = slowerPointer.next;
+            }
+
+            fasterPointer = fasterPointer.next;
+            counter++;
+        }
+
+        if(counter == 0){
+            return new Integer[]{};
+        }
+
+        ArrayList<Integer> arrList = new ArrayList<>();
+        if(counter % 2 == 0){
+            arrList.add(slowerSlowerPointer.item);
+        }
+
+        arrList.add(slowerPointer.item);
+
+        return arrList.toArray(new Integer[0]);
+    }
+
+    private Node getPrevious(Node node) {
+        Node current = first;
+
+        while(current != null){
+            if(current.next == node){
+                return current;
+            }
+
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    private boolean isEmpty(){
+        return size == 0;
+    }
+
+    public boolean hasLoop() {
+
+        Node slow = first;
+        Node fast = first;
+
+        do {
+
+            if(fast == null){
+                break;
+            }
+
+            fast = fast.next;
+
+            if(fast == slow){
+                break;
+            }
+
+            if(fast != null){
+                fast = fast.next;
+            }
+            slow = slow.next;
+
+        }while(fast != null || slow != fast);
+
+        return slow == fast;
+    }
+
+    public void addLoop(int from, int to){
+
+        if(isEmpty()){
+            throw new IllegalStateException("Can not set a loop in an empty  list.");
+        }
+
+        if(from < 0 || to < 0){
+            throw new IllegalArgumentException("From or to less than 0.");
+        }
+
+        if(from < to){
+            throw new IllegalArgumentException("'From' is less than 'to'");
+        }
+
+        if(from >= size){
+            throw new IllegalArgumentException("from is larger or equal to list size (" + size + ")");
+        }
+
+        int index = 0;
+        Node fromNode = null;
+        Node toNode = null;
+
+        Node current = first;
+
+        while((fromNode == null && toNode == null) || current != null){
+
+            if(index == from){
+                fromNode = current;
+            }
+
+            if(index == to){
+                toNode = current;
+            }
+
+            index++;
+            current = current.next;
+        }
+
+        size = index + 1;
+        fromNode.next = toNode;
+        last = null;
+    }
+
+    @Override
+    public String toString(){
+
+        StringBuilder sb = new StringBuilder("[");
+
+        Node current = first;
+
+        while(current != null){
+
+            sb.append(current);
+            sb.append(current != last? ", ": "");
+            current = current.next;
+        }
+
+        sb.append("]");
+
+        return sb.toString();
+    }
+}
---
+++ Part_1/src/test/java/pl/lukaszJaskiewicz/LinkedListTest.java
@@ -0,0 +1,277 @@
+package pl.lukaszJaskiewicz;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    LinkedList list;
+
+    @BeforeEach
+    void prepare() {
+        list = new LinkedList();
+    }
+
+    @Test
+    void addFirst_whenCalled_shouldAddItemAtListStart() {
+
+        assertEquals(list.toString(), "[]");
+
+        list.addFirst(30);
+        assertEquals(list.toString(), "[30]");
+
+        list.addFirst(20);
+        assertEquals(list.toString(), "[20, 30]");
+
+        list.addFirst(10);
+        assertEquals(list.toString(), "[10, 20, 30]");
+    }
+
+    @Test
+    void addLast_whenCalled_shouldAddItemAtListEnd() {
+
+        list.addLast(10);
+        assertEquals(list.toString(), "[10]");
+
+        list.addLast(20);
+        assertEquals(list.toString(), "[10, 20]");
+
+        list.addLast(30);
+        assertEquals(list.toString(), "[10, 20, 30]");
+    }
+
+    @Test
+    void deleteFirst_whenCalled_shouldDeleteFirstItemFromList() {
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+
+        assertEquals(list.toString(), "[10, 20, 30]");
+
+        list.deleteFirst();
+        assertEquals(list.toString(), "[20, 30]");
+
+        list.deleteFirst();
+        assertEquals(list.toString(), "[30]");
+
+        list.deleteFirst();
+        assertEquals(list.toString(), "[]");
+
+
+    }
+
+    @Test
+    void deleteFirst_whenCalled_shouldThrowNoSuchElementException() {
+        assertThrows(NoSuchElementException.class, list::deleteFirst);
+    }
+
+    @Test
+    void deleteLast_whenCalled_shouldDeleteLastItemFromList(){
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+
+        list.deleteLast();
+        assertEquals(list.toString(), "[10, 20]");
+
+        list.deleteLast();
+        assertEquals(list.toString(), "[10]");
+
+        list.deleteLast();
+        assertEquals(list.toString(), "[]");
+    }
+
+    @Test
+    void deleteLast_whenCalled_shouldThrowNoSuchElementException() {
+        assertThrows(NoSuchElementException.class, list::deleteLast);
+    }
+    
+    @Test
+    void indexOf_whenCalled_shouldReturnIndexOfFirstPresentItem() {
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(10);
+
+        assertEquals(list.indexOf(10), 0);
+        assertEquals(list.indexOf(20), 1);
+        assertEquals(list.indexOf(30), 2);
+        assertEquals(list.indexOf(0), -1);
+        assertEquals(list.indexOf(40), -1);
+    }
+
+    @Test
+    public void contains_whenCalled_shouldReturnTrue() {
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(10);
+
+        assertTrue(list.contains(10));
+        assertTrue(list.contains(20));
+        assertTrue(list.contains(30));
+
+        assertFalse(list.contains(0));
+        assertFalse(list.contains(40));
+    }
+
+    @Test
+    public void toArray_whenCalled_shouldReturnArrayOfList(){
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(10);
+
+        assertArrayEquals(list.toArray(), new int[]{10, 20, 30});
+
+        list.deleteLast();
+        assertArrayEquals(list.toArray(), new int[]{10, 20});
+
+        list.deleteFirst();
+        assertArrayEquals(list.toArray(), new int[]{20});
+
+        list.deleteFirst();
+        assertArrayEquals(list.toArray(), new int[]{});
+    }
+
+    @Test
+    public void reverse_whenCalled_shouldReverseList() {
+
+        list.addLast(10);
+
+        list.reverse();
+        assertArrayEquals(list.toArray(), new int[] {10});
+
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+
+        list.reverse();
+
+        assertArrayEquals(list.toArray(), new int[] {50, 40, 30, 20, 10});
+    }
+
+    @Test
+    public void getKthFromTheEnd_whenCalled_shouldReturnKthNodeFromTheEnd() {
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+
+        assertEquals(10, list.getKthFromTheEnd(5));
+        assertEquals(20, list.getKthFromTheEnd(4));
+        assertEquals(30, list.getKthFromTheEnd(3));
+        assertEquals(40, list.getKthFromTheEnd(2));
+        assertEquals(50, list.getKthFromTheEnd(1));
+        assertEquals(50, list.getKthFromTheEnd(0));
+        assertEquals(50, list.getKthFromTheEnd(-1));
+    }
+
+    @Test
+    public void getKthFromTheEnd_whenCalled_shouldThrowArgumentException() {
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+
+        assertThrows(IllegalArgumentException.class, () -> list.getKthFromTheEnd(6));
+    }
+
+    @Test
+    public void getKthFromTheEnd_whenCalled_shouldThrowIllegalStateException() {
+        assertThrows(IllegalStateException.class, () -> list.getKthFromTheEnd(1));
+    }
+
+    @Test
+    public void getMiddle_whenCalled_shouldReturnEmptyArray() {
+        assertArrayEquals(new Integer[] {}, list.getMiddle());
+    }
+
+    @Test
+    public void getMiddle_whenCalled_shouldReturnFirstItem() {
+        list.addLast(10);
+
+        assertArrayEquals(new Integer[] {10}, list.getMiddle());
+    }
+
+    @Test
+    public void getMiddle_whenCalled_shouldReturnFirstAndSecondItem() {
+
+        list.addLast(10);
+        list.addLast(20);
+
+        assertArrayEquals(new Integer[]{ 10, 20 }, list.getMiddle());
+    }
+
+    @Test
+    public void getMiddle_whenCalled_shouldReturnSingleMiddleItem(){
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+
+        assertArrayEquals(new Integer[] {30}, list.getMiddle());
+    }
+
+    @Test
+    public void getMiddle_whenCalled_shouldReturnPairOfMiddleItems(){
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+        list.addLast(60);
+
+        assertArrayEquals(new Integer[] { 30, 40 }, list.getMiddle());
+    }
+
+    @Test
+    public void hasLoop_whenCalled_returnsTrue(){
+
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+        list.addLast(60);
+
+        list.addLoop(5, 2);
+
+        assertTrue(list.hasLoop());
+    }
+
+    @Test
+    public void hasLoop_whenCalled_returnFalse(){
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+        list.addLast(60);
+
+        assertFalse(list.hasLoop());
+    }
+
+    @Test
+    public void hasLoop_whenCalled_throwsException() {
+
+        assertThrows(IllegalStateException.class, () -> list.addLoop(2,1));
+
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+
+        assertThrows(IllegalArgumentException.class, () -> list.addLoop(-1,1));
+        assertThrows(IllegalArgumentException.class, () -> list.addLoop(2,-1));
+
+        assertThrows(IllegalArgumentException.class, () -> list.addLoop(1, 2));
+
+        assertThrows(IllegalArgumentException.class, () -> list.addLoop(4,1));
+    }
+}
